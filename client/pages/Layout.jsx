a1ef757140208bd133ab1417ad1cd32a916c8c5d
krAryn <aj619119orp@gmail.com>
2025-08-02 16:50:09 +0530

    Initialize Project

---
+++ client/pages/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+      Layout Page
+    </div>
+  )
+}
+
+export default Layout
---
+++ client/pages/Login.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { assets } from '../src/assets/assets';
+import { Star } from 'lucide-react';
+
+const Login = () => {
+  return (
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <img src={assets.bgImage} className='absolute top-0 left-0 -z-1 w-full h-full object-cover' alt="" />
+
+      <div className='flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40'>
+        <img src={assets.logo} className='h-12 object-contain' alt="" />
+        <div>
+          <div className='flex items-center gap-3 mb-4 max-md:mt-10'>
+            <img src={assets.group_users} className='h-8 md:h-10' alt="" />
+            <div>
+              <div className='flex'>
+                {Array(5).fill(0).map((_, i) => (<Star key={i} className='size-4 md:size-4.5 text-transparent fill-amber-500' />))}
+              </div>
+              <p>Used by 12k+ developers</p>
+            </div>
+          </div>
+
+          <h1 className='text-3xl md:text-6xl md:pb-2 font-bold bg-gradient-to-r from-indigo-950 to-indigo-800 bg-clip-text text-transparent'>For the ones, who matter</h1>
+          <p className='text-xl mx:text-3xl text-indigo-900 max-w-72'>Bring you closed ones to you Nest.</p>
+        </div>
+      </div>
+
+      
+
+    </div>
+  );
+};
+
+export default Login;
