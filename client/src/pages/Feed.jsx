685f3e8de3e5bbb9cf2738c41434cdc646788f24
krAryn <aj619119orp@gmail.com>
2025-08-05 22:07:17 +0530

    Create Post Card

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { dummyPostsData } from '../assets/assets';
+import React, { useEffect, useState, useRef } from 'react';
+import { assets, dummyPostsData } from '../assets/assets';
 import Loader from '../components/Loader';
 import StoriesBar from '../components/StoriesBar';
 import { MessageSquareQuote } from 'lucide-react';
+import PostCard from '../components/PostCard';
 
 const Feed = () => {
 
   const [feeds, setFeeds] = useState()
+  const recentMessagesBtnRef = useRef()
 
   const fetchFeeds = async () => {
     setFeeds(dummyPostsData)
@@ -16,13 +18,12 @@ const Feed = () => {
   let isRecentMessagesVisible = false;
   const toggleRecentMessages = () => {
     console.log(isRecentMessagesVisible)
-    const ele = document.querySelector(".recent-messages")
 
     if (!isRecentMessagesVisible) {
-      ele.style.transform = "translateY(0)"
+      recentMessagesBtnRef.current.style.transform = "translateY(-100%)"
       isRecentMessagesVisible = true;
     } else {
-      ele.style.transform = "translateY(60%)";
+      recentMessagesBtnRef.current.style.transform = "translateY(0)";
       isRecentMessagesVisible = false
     }
   }
@@ -37,23 +38,28 @@ const Feed = () => {
 
         <div className='w-full overflow-x-hidden overflow-y-auto h-full pt-15'>
           <StoriesBar />
-          <div className='px-4 py-4 h-fit'>
-            List of Posts
+          <div className='px-4 py-4 h-fit flex flex-col gap-4'>
+            {feeds.map((post) => (
+              <PostCard key={post._id} post={post} />
+            ))}
           </div>
         </div>
 
 
-        <div className='recent-messages max-xl:absolute max-xl:bottom-2 max-xl:right-6 max-xl:w-[300px] xl:relative transition-all flex flex-col gap-3 min-w-[300px]' style={{ transform: "translateY(60%)" }}>
+        <div ref={recentMessagesBtnRef} className='recent-messages max-xl:absolute max-xl:bottom-14 max-xl:right-6 max-xl:w-[300px] xl:relative max-xl:translate-y-[100%] transition-all flex flex-col gap-3 min-w-[300px]'>
           <div className='h-10 xl:hidden relative invisible'></div>
-          <div className='toggle-button bg-primary text-white rounded-full p-4 xl:hidden absolute right-0 -top-4 transition-all cursor-pointer active:scale-90'
+          <div className='toggle-button bg-primary text-white rounded-full p-4 xl:hidden absolute right-0 -top-2 transition-all cursor-pointer active:scale-90'
             onClick={toggleRecentMessages}
           >
             <MessageSquareQuote size={20} />
           </div>
-          <div className='main border rounded-lg p-4 '>
-            <h1>Sponsored</h1>
-            <h1>Recent Messages</h1>
+          <div className={`main max-w-xs text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow border border-gray-200 bg-slate-100`}>
+            <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+            <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+            <p className='text-slate-600'>Email marketing</p>
+            <p className='text-slate-400'>Take your Business off the charts using your Nest for marketing</p>
           </div>
+            <h1>Recent Messages</h1>
         </div>
 
       </div>
