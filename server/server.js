bd9b40bf48a211ed4e50f0caf648ef4996cb6f7e
krAryn <aj619119orp@gmail.com>
2025-08-31 18:58:32 +0530

    Initialize Server

--- server/server.js
+++ server/server.js
@@ -5,3 +5,13 @@ import {join, dirname} from "path"
 import { fileURLToPath } from "url";
 
 dotenv.config({path: join(dirname(fileURLToPath(import.meta.url)), "envDir", ".env")})
+const PORT = process.env.PORT || 4000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log("Server is Running on Port: ", PORT)
+})
